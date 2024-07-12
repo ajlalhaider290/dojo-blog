26be1c705b683c2cb25377f1165d541044f1a181
Ajlalhaider <ajlal.haider@codeninjaconsulting.com>
2024-07-12 15:05:28 +0500

    [ADD] added react routing

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,7 +7,10 @@ const useFetch = (url) => {
 
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+
+
+        fetch(url, { signal: abortCont.signal })
         .then(res => {
             if(!res.ok) {
                 throw Error('Could not fetch data for that resource');
@@ -20,9 +23,16 @@ const useFetch = (url) => {
             setError(null);
         })
         .catch(err => {
-            setIsPending(false);
-            setError(err.message);
+            if (err.name !== 'AbortError') {
+                setIsPending(false);
+                setError(err.message);
+            }
+            else {
+                console.log('fetch aborted');
+            }
         })
+        
+        return () => abortCont.abort();
     }, [url]);
 
     return {data, isPending, error}
